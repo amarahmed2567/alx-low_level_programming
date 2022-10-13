4a21d626018128522ba3dfbc5dd4b2090e4a57ea
amarahmed2567 <amarahmed2567@gmail.com>
2022-10-13 12:52:17 -0700

    added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * * print with puts
+ * * don't wory
+ * */
+int main()
+{
+	    char Name1[] = "\"Programming is like building a multilingual puzzle";
+	        puts(Name1);
+		    return 0;
+}
