cbbb6190a674f0e7afd29a2b067d782ac75f5f9b
Amar Ahmed <74867631+amarahmed2567@users.noreply.github.com>
2022-10-13 21:37:58 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-  /**
-  *Print Hello wolrd
-  *
-  *retutn 0
-  */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return 0;
 }
