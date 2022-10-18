cc31205ad53fcf4e2ce276609d1fc5f06bbc1a0f
Amar Ahmed <amarahmed2567@gmail.com>
2022-10-18 23:30:07 +0200

    added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,20 @@ int print_sign(int n)
 if (n == 0)
 {
 _putchar('0');
+_putchar('\n');
 return (0);
 }
 else if (n > 0)
 {
 _putchar('+');
+_putchar('\n');
 return (1);
 }
 else
 {
 _putchar('-');
+_putchar('\n');
 return (-1);
 }
-_putchar('\n');
+
 }
