d893e4645353e7458e3e7d9b8eeb6bcd726f70a0
Amar Ahmed <74867631+amarahmed2567@users.noreply.github.com>
2022-10-18 21:28:47 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-
 void print_alphabet(void){
+    
     /*
     *Print Alphabet with 28char
     *end with new line 
     */
-    char c;
-
-    for (c = 'a'; c <= 'z'; ++c)
-    {
+    
+char c;
+for (c = 'a'; c <= 'z'; ++c)
     _putchar(c);
-    }
     _putchar('\n');
-
 }
