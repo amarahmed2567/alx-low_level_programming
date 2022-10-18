4b10b1c238b13ed5cac4af9799acddcb64a9090f
Amar Ahmed <74867631+amarahmed2567@users.noreply.github.com>
2022-10-18 20:14:13 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,14 @@ int main(void){
 
 //print _putchar
 
-_putchar("_/n");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r\n");
 
 return 0;
 
