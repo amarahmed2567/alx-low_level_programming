29d11cd7590b3fb6f67cae2d118145245ab11ea0
Amar Ahmed <amarahmed2567@gmail.com>
2022-10-18 23:27:15 +0200

    added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,24 @@
 #include "main.h"
-
 /**
- * print_sign - return 0 letter not lowercase, 1 letter lowercase
- *
- *@n: the int to print
- * Return: Always 0.
- */
+*print_sign-prints sign of a number
+*@n: character to be verified
+*Return: return 0 or 1
+*/
 int print_sign(int n)
 {
-if (n > 0)
+if (n == 0)
 {
-_putchar ('+');
-return (1);
-}
-
-else if (n == 0)
-{
-_putchar ('0');
+_putchar('0');
 return (0);
 }
-
+else if (n > 0)
+{
+_putchar('+');
+return (1);
+}
 else
 {
-_putchar ('-');
+_putchar('-');
 return (-1);
 }
-
 }
