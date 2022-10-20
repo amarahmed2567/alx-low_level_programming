73d265a1f279ca6154391cafa361016b014900f5
Amar Ahmed <amarahmed2567@gmail.com>
2022-10-20 18:39:47 +0200

    added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+
+if(_isupper(c)){
+    return (1);
+}else{
+    return (0);
+}
+
+}
