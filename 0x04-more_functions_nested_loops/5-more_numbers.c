6aeb0148d3288bca0fac864ad8156e90e3462703
Amar Ahmed <amarahmed2567@gmail.com>
2022-10-20 21:33:52 +0200

    added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,12 @@ void print_most_numbers(void)
 {
 	int number;
 
-	for (number = '0'; number <= '9'; number++)
+	for (number = 0; number <= 10; number++)
 	{
-		if (number == '2' || number == '4')
-			;
-		else
-			_putchar(number);
+        int fnum;
+        for(fnum='0'; fnum<='14';fnum++){
+	        _putchar(fnum);
+        }
 	}
 	_putchar('\n');
 }
