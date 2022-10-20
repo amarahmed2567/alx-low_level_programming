b6c4ea8e7a99f12d015b563e072b2dca14aea9fe
Amar Ahmed <amarahmed2567@gmail.com>
2022-10-20 22:17:41 +0200

    added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int n;
+	for (n = 1; n <= 10; n++)
+    {
+        _putchar('_');
+    }
+    	_putchar('\n');
+}
