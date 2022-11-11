ac0377ff9fa9856a226bb235f34d2bce76c759fe
Amar Ahmed <amarahmed2567@gmail.com>
2022-11-11 20:40:59 +0200

    added

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b){
+    int *ptr ;
+    ptr = malloc(b);
+    if(ptr == NULL){
+        exit(98);
+    }
+}
